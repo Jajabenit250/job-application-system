66ea18664aa5f2d87f214b92e0909b7b703cf7f9
Havugimana Benit <jajabenit@gmail.com>
2022-03-16 17:56:07 +0200

    final changes

--- client/src/controllers/ApplicantTable.js
+++ client/src/controllers/ApplicantTable.js
@@ -1,10 +1,12 @@
 import * as React from "react";
+import { useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { connect } from "react-redux";
+import { applicantsByJob } from "../redux/actions";
 
 const style = {
   position: "absolute",
@@ -18,48 +20,82 @@ const style = {
   p: 4,
 };
 
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.getValue(params.id, "firstName") || ""} ${
-        params.getValue(params.id, "lastName") || ""
-      }`,
-  },
-];
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
 
-function DataTable() {
+
+
+function DataTable(props) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const columns = [
+    { field: "_id", headerName: "ID", minWidth: 70 },
+    { field: "applicantName", headerName: "applicant Name", minWidth: 200 },
+    { field: "coverLetter", headerName: "cover Letter", minWidth: 150 },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      minWidth: 150,
+    },
+    {
+      field: "degree",
+      headerName: "degree",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      minWidth: 200,
+    },
+  
+    {
+      field: "fileURL",
+      headerName: "Resume",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      minWidth: 200,
+    },
+    {
+      field: "Status",
+      headerName: "Status",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      minWidth: 150,
+    },
+  
+    {
+      field: "_first_login",
+      headerName: "Action",
+      sortable: false,
+      flex: 0.5,
+      minWidth: 100,
+      renderCell: (params) => {
+        const onClick = (e) => {
+          handleOpen(true);
+          e.stopPropagation(); // don't select this row after clicking
+  
+          const api = params.api;
+          const thisRow = {};
+  
+          api
+            .getAllColumns()
+            .filter((c) => c.field !== "__check__" && !!c)
+            .forEach(
+              (c) => (thisRow[c.field] = params.getValue(params.id, c.field))
+            );
+          return handleOpen(true);
+        };
+  
+        return <Button onClick={onClick}>View Details</Button>;
+      },
+    },
+  ];
+  useEffect(() => {
+    props.applicantsByJob();
+  }, []);
   return (
     <>
-      <Button onClick={handleOpen}>Open modal</Button>
+      {/* <Button onClick={handleOpen}>Open modal</Button> */}
+      <br /> 
       <Modal
         open={open}
         onClose={handleClose}
@@ -76,13 +112,15 @@ function DataTable() {
         </Box>
       </Modal>
       <div style={{ height: 400, width: "100%" }}>
-        <DataGrid
-          rows={rows}
+      {props.allVacancy ? <DataGrid
+          rows={props.allVacancy.datas }
           columns={columns}
           pageSize={5}
+          getRowId={() => Math.random()}
           rowsPerPageOptions={[5]}
           checkboxSelection
-        />
+        /> : "" }
+        
       </div>{" "}
     </>
   );
@@ -90,12 +128,12 @@ function DataTable() {
 
 const mapStateToProps = (state) => {
   return {
-    allVacancy: state.vacancy.data,
+    allVacancy: state.applicantList.data,
   };
 };
 const mapDispatchToProps = (dispatch) => {
   return {
-    // uploadCvAction: (data) => dispatch(uploadCvAction(data)),
+    applicantsByJob: (data) => dispatch(applicantsByJob(data)),
   };
 };
 
--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -8,7 +8,7 @@ export const uploadCvAction = (data) => async (dispatch) => {
       },
     };
     const res = await axios.post(
-      `http://localhost:5000/api/applicant/apply`,
+      `https://job-appl.herokuapp.com/api/applicant/apply`,
       data,
       config
     );
@@ -28,7 +28,7 @@ export const uploadCvAction = (data) => async (dispatch) => {
 
 export const getVacancies = () => async (dispatch) => {
   try {
-    const res = await axios.get(`http://localhost:5000/api/job/all`);
+    const res = await axios.get(`https://job-appl.herokuapp.com/api/job/all`);
     console.log(res.data);
 
     dispatch({ type: "VACANCIES", payload: res.data });
@@ -45,4 +45,19 @@ export const getVacancies = () => async (dispatch) => {
 
 export const applyVacancy = (data) => async (dispatch) => {};
 
-export const applicantsByJob = (data) => async (dispatch) => {};
+export const applicantsByJob = (data) => async (dispatch) => {
+  try {
+    const res = await axios.get(`https://job-appl.herokuapp.com/api/applicant/all/6231d9ad6087cc81bc6cc6d1`);
+    console.log(res.data);
+
+    dispatch({ type: "APPLICANTS", payload: res.data });
+  } catch (err) {
+    if (err.response) {
+      const errorMessage = await err.response.data.errors;
+      dispatch({ type: "APPLICANTS_ERROR", payload: err.response.data.errors });
+      console.log(errorMessage);
+    } else {
+      dispatch({ type: "APPLICANTS_ERROR", payload: "Network Error" });
+    }
+  }
+};
